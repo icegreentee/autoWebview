6d937ca78a8bffdaebb70c8be895d8eba7c0a7ef
star sky <1176789241@qq.com>
2021-05-15 19:49:04 +0800

    feat: 仓库初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mu-form
+      :model="form"
+      class="mu-demo-form"
+      :label-position="labelPosition"
+      label-width="100"
+    >
+      <mu-form-item prop="input" label="Input">
+        <mu-text-field v-model="form.input"></mu-text-field>
+      </mu-form-item>
+      <mu-form-item prop="select" label="Select">
+        <mu-select v-model="form.select">
+          <mu-option
+            v-for="option in options"
+            :key="option"
+            :label="option"
+            :value="option"
+          ></mu-option>
+        </mu-select>
+      </mu-form-item>
+      <mu-form-item prop="date" label="Date Time">
+        <mu-date-input
+          v-model="form.date"
+          type="dateTime"
+          actions
+        ></mu-date-input>
+      </mu-form-item>
+      <mu-form-item prop="radio" label="Radio">
+        <mu-radio v-model="form.radio" value="male" label="Male"></mu-radio>
+        <mu-radio v-model="form.radio" value="female" label="Female"></mu-radio>
+      </mu-form-item>
+      <mu-form-item prop="checkbox" label="Checkbox">
+        <mu-checkbox
+          v-model="form.checkbox"
+          value="eat"
+          label="Eat"
+        ></mu-checkbox>
+        <mu-checkbox
+          v-model="form.checkbox"
+          value="sleep"
+          label="Sleep"
+        ></mu-checkbox>
+        <mu-checkbox
+          v-model="form.checkbox"
+          value="run"
+          label="Run"
+        ></mu-checkbox>
+        <mu-checkbox
+          v-model="form.checkbox"
+          value="movie"
+          label="Movie"
+        ></mu-checkbox>
+      </mu-form-item>
+      <mu-form-item prop="switch" label="Switch">
+        <mu-switch v-model="form.switch"></mu-switch>
+      </mu-form-item>
+      <mu-form-item prop="slider" label="Slider">
+        <mu-slider v-model="form.slider"></mu-slider>
+      </mu-form-item>
+      <mu-form-item prop="textarea" label="Textarea">
+        <mu-text-field
+          multi-line
+          :rows="3"
+          :rows-max="6"
+          v-model="form.textarea"
+        ></mu-text-field>
+      </mu-form-item>
+    </mu-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      options: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 7",
+        "Option 8",
+        "Option 9",
+        "Option 10",
+      ],
+      labelPosition: "top",
+      form: {
+        input: "",
+        select: "",
+        date: "",
+        radio: "",
+        checkbox: [],
+        switch: false,
+        slider: 30,
+        textarea: "",
+      },
+    };
+  },
+};
 </script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+.mu-demo-form {
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// ui库
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+Vue.use(MuseUI);
+// FastClick
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+// flexible
+import '../public/js/flexible.js'
 
 Vue.config.productionTip = false
 
